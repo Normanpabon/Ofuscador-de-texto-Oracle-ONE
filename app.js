1b7354983094d1f70adb4d7d69ef50c758e57c48
Norman P <45250874+Normanpabon@users.noreply.github.com>
2024-01-30 14:15:53 -0500

    Logica finalizada
    - Se termina comportamiento de cifrado y decifrado.
    
    - Se conectan los elementos html faltantes.
    
    pendiente:
    - Realizar ajustes de accesibilidad
    - Ajustar el css

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 /*https://www.aluracursos.com/challenges/challenge-one-logica/sprint01-construye-un-encriptador-texto-con-javascript*/
-
-/*
-La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat"
-*/
-
 /**https://sweetalert2.github.io/ */
+
+// Configuracion inicial
+document.getElementById("divMensajeDesencriptado").style.display = "none"; 
 
 function moduloCifrado(frase){
     let fraseConCambio = "";
@@ -42,12 +36,52 @@ function moduloCifrado(frase){
     return fraseConCambio;
 }
 
+/*
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
+
 function moduloDecifrado(frase){
-    return "No implementado aun";
+
+    let fraseBk = frase;
+    //Regex
+    const regE = /enter/g;
+    const regI = /imes/g;
+    const regA = /ai/g;
+    const regO = /ober/g;
+    const regU = /ufat/g;
+
+
+    frase = frase.replace(regE, "e")
+    frase = frase.replace(regI, "i");
+    frase = frase.replace(regA, "a");
+    frase = frase.replace(regO, "o");
+    frase = frase.replace(regU, "u");
+
+    if(frase != fraseBk){
+
+        try {
+            document.getElementById("mensajeDefault").style.display = "none"; 
+            document.getElementById("divMensajeDesencriptado").style.display = "inline-block"; 
+        } catch (error) {
+            console.log("Error: "+ error);   
+        }
+
+        let textAreaDesencriptado = document.getElementById("mensajeDesencriptado");
+        textAreaDesencriptado.innerHTML = frase;
+
+    }
+
+    return frase;
 }
 
 function encriptar(){
-    alert("Boton encriptar pulsado");
+
+    document.getElementById("mensajeDefault").style.display = "inline-block";
+    document.getElementById("divMensajeDesencriptado").style.display = "none"; 
 
     let textoIngresado = document.getElementById("mensaje").value;
 
@@ -58,7 +92,6 @@ function encriptar(){
 }
 
 function desencriptar(){
-    alert("Boton desencriptar pulsado");
 
     let textoIngresado = document.getElementById("mensaje").value;
 
